e70658e5815f3a9a77da540d82862903df10411d
mariasolegallina <gallinamariasole@gmail.com>
2024-04-05 17:30:26 +0200

    add page "contatti"

---
+++ resources/views/contatti.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contatti</title>
+</head>
+
+<body>
+    <h1>Contatti</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A quia sed commodi quaerat impedit eaque minus totam, enim aliquid fugiat ullam laudantium, cumque corporis dolor tempore rem nobis nam quisquam.</p>
+
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+Route::get('contatti', function () {
+    return view('contatti');
+});
